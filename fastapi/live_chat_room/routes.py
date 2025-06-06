efa52a37fe95163abf4f954d2886fa400410c35f
Adnan <adnanmuhammedadnancp@gmail.com>
2025-06-06 11:30:59 +0530

    add logout route

--- fastapi/live_chat_room/routes.py
+++ fastapi/live_chat_room/routes.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, status, Cookie, Response
+from fastapi.responses import JSONResponse
+from sqlalchemy import JSON
 from sqlalchemy.orm import Session
 from live_chat_room.schemas import UserSchema
 from live_chat_room.db import get_session
@@ -9,26 +11,52 @@ router = APIRouter()
 
 
 @router.post("/login")
-def login(user: UserSchema, session: Session = Depends((get_session))):
+def login(user: UserSchema, session: Session = Depends((get_session))) -> JSONResponse:
     avail_user = session.query(UserModel).filter(UserModel.name == user.name).first()
     if not avail_user:
-        raise HTTPException(status_code=401, detail="authorization error!")
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="authorization error!"
+        )
 
     is_pass_match = verifiy_passwd(passwd=user.password, hash=avail_user.password)
     if not is_pass_match:
-        raise HTTPException(status_code=401, detail="authorization error!")
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="authorization error!"
+        )
 
     token = gen_access_token({"sub": user.name})
-    return {"message": "logged in successfull!", "access_token": token}
+    response = JSONResponse(
+        {"message": "successfully logged in!", "access_token": token}
+    )
+    response.set_cookie(key="acc_token", value=token, httponly=True)
+    return response
 
 
 @router.post("/signup")
 def signup(user: UserSchema, session: Session = Depends((get_session))):
     existing_user = session.query(UserModel).filter(UserModel.name == user.name).first()
     if existing_user:
-        raise HTTPException(status_code=409, detail="user already exist!")
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT, detail="user already exist!"
+        )
 
     new_user = UserModel(name=user.name, password=hash_passwd(user.password))
     session.add(new_user)
     session.commit()
     return {"message": f"{new_user.name} created successfully!"}
+
+
+@router.post("/logout")
+def logout(response: Response):
+    response = JSONResponse({"message": "Logged out successfully!"})
+    response.delete_cookie("acc_token")
+    return response
+
+
+@router.get("/show")
+def get_token(acc_token=Cookie(default=None)):
+    if not acc_token:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="token not avilable"
+        )
+    return {"token": acc_token}
