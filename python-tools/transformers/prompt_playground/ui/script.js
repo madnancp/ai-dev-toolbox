1335ff82350eb0c2e6cb4e10ba873c7767e361a4
Adnan <adnanmuhammedadnancp@gmail.com>
2025-06-13 20:25:46 +0530

    add max_new_tokens field

--- python-tools/transformers/prompt_playground/ui/script.js
+++ python-tools/transformers/prompt_playground/ui/script.js
@@ -8,12 +8,14 @@ promptForm.addEventListener("submit", (event) => {
   const temperature = formData.get("temp");
   const topK = formData.get("top-k");
   const topP = formData.get("top-p");
+  const maxNewTokns = formData.get("max-new-tokens");
 
   const data = {
     prompt: prompt,
     temperature: parseFloat(temperature),
     top_k: parseInt(topK),
     top_p: parseFloat(topP),
+    max_new_tokens: parseInt(maxNewTokns),
   };
 
   fetch("http://localhost:8000/api/v1/generate", {
