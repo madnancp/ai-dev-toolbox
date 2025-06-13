d5ea68101812a0a4bbc6eb5eda43ac66dc4672a8
Adnan <adnanmuhammedadnancp@gmail.com>
2025-06-13 17:07:12 +0530

    add model infr manager on fastapi startup

--- python-tools/transformers/prompt_playground/routes.py
+++ python-tools/transformers/prompt_playground/routes.py
@@ -1,5 +1,7 @@
-from fastapi import APIRouter
+from contextlib import contextmanager
+from fastapi import APIRouter, FastAPI
 from pydantic import BaseModel
+from prompt_playground.inference import LLMInferenceManager
 
 
 class PromptSchema(BaseModel):
@@ -9,9 +11,21 @@ class PromptSchema(BaseModel):
     top_p: float
 
 
+llm_inference: None | LLMInferenceManager = None
+
+
+@contextmanager
+async def lifespan(app: FastAPI):
+    global llm_inference
+    llm_inference = LLMInferenceManager()
+    yield
+
+
 router = APIRouter(tags=["inference"])
 
 
 @router.post("/generate")
 def get_llm_reponse(prompt: PromptSchema):
+    if llm_inference is not None:
+        llm_inference.ask(prompt.prompt)
     return {"result": "Capital of India is New Delhi."}
